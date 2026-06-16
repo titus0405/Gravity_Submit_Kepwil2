68696df62c4b913def2a36f75deb4a1373535aa9
titus0405 <ckbatam0405@gmail.com>
2026-06-16 22:53:39 +0700

    update new

--- bpjs-kesehatan-new/src/app/core/services/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './content.service';
-export * from './navigation.service';
--- bpjs-kesehatan-new/src/app/shared/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { ContentCardComponent } from './content-card/content-card.component';
-export { ServiceCardComponent } from './service-card/service-card.component';
-export { ToolCardComponent } from './tool-card/tool-card.component';
-export { SectionHeadingComponent } from './section-heading/section-heading.component';
-export { SearchBarComponent } from './search-bar/search-bar.component';
-export { SkeletonComponent } from './skeleton/skeleton.component';
-export { BannerCarouselComponent } from './banner-carousel/banner-carousel.component';
